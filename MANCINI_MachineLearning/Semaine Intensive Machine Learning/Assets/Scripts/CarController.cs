6d7c52d93569ec850c73c9d69bb601f4908764e9
Léonard <epoc4oftime@gmail.com>
2020-03-02 16:21:29 +0100

    SemaineIntensive_Machine Learning

---
+++ MANCINI_MachineLearning/Semaine Intensive Machine Learning/Assets/Scripts/CarController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private void Start()
+    {
+            
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
