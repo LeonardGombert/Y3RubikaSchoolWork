0daafda48c3160c41a16df7acc83a12c5afed7e6
Léonard <epoc4oftime@gmail.com>
2020-03-03 17:05:04 +0100

    MachineLearning Day 2

--- MANCINI_MachineLearning/Semaine Intensive Machine Learning/Assets/Scripts/Agent.cs
+++ MANCINI_MachineLearning/Semaine Intensive Machine Learning/Assets/Scripts/Agent.cs
@@ -3,12 +3,152 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Agent : MonoBehaviour
+public class Agent : MonoBehaviour, IComparable<Agent>
 {
+    public NeuralNetwork net;
+    //the higher this value is, the better (total distance)
+    public float fitness;
+
     public Transform nextCheckpoint;
+    public float nextCheckpointDist;
+    //distance since last checkpoint (different from fitness)
+    public float distanceTraveled;
 
-    public void CheckpointReached(Transform nextCheckpoint)
+    public MeshRenderer render;
+
+    public Material defaultMat;
+    public Material mutantMat;
+    public Material firstMat;
+
+    public Transform tr;
+    public Rigidbody rb;
+
+    public float[] inputs;
+
+    Vector3 pos;
+    RaycastHit hit;
+
+    public float rayRange = 1;
+
+    public LayerMask layerMask;
+
+    public CarController carController;
+
+    public void CheckpointReached(Transform newNextCheckpoint)
     {
+        distanceTraveled += nextCheckpointDist;
+        nextCheckpoint = newNextCheckpoint;
+        nextCheckpointDist = (transform.position - nextCheckpoint.position).magnitude;
+    }
 
+    public void SetDefaultColor()
+    {
+        render.material = defaultMat;
+    }
+
+    public void SetMutatedColor()
+    {
+        render.material = mutantMat;
+    }
+
+    public void SetFirstColor()
+    {
+        render.material = firstMat;
+    }
+
+    public int CompareTo(Agent other)
+    {
+        if (fitness < other.fitness)
+        {
+            return 1;
+        }
+
+        else if (fitness > other.fitness)
+        {
+            return -1;
+        }
+
+        else return 0;
+    }
+
+    internal void ResetAgent()
+    {
+        fitness = 0;
+        tr.position = Vector3.zero;
+        tr.rotation = Quaternion.identity;
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        distanceTraveled = 0;
+
+        nextCheckpoint = CheckpointManager.instance.firstCheckPoint;
+        nextCheckpointDist = (tr.position - nextCheckpoint.position).magnitude;
+
+        inputs = new float[net.layers[0]];
+    }
+
+    private void FixedUpdate()
+    {
+        InputUpdate();
+        OutputUpdate();
+        UpdateFitness();
+    }
+
+
+    private void InputUpdate()
+    {
+        pos = transform.position;
+        inputs[0] = RaySensor(pos + new Vector3(0, .2f, 0), transform.forward, 4);
+        inputs[1] = RaySensor(pos + new Vector3(0, .2f, 0), transform.right + transform.forward, 2);
+        inputs[2] = RaySensor(pos + new Vector3(0, .2f, 0), -transform.right + transform.forward, 2);
+        inputs[3] = RaySensor(pos + new Vector3(0, .2f, 0), transform.right, 1);
+        inputs[4] = RaySensor(pos + new Vector3(0, .2f, 0), -transform.right, 1);
+        
+        //horizontal and vertical inputs
+        inputs[5] = 1 - (float)Math.Tanh(rb.velocity.magnitude / 20);
+        inputs[6] = (float) Math.Tanh(rb.angularVelocity.y * 0.01f);
+    }
+    private void OutputUpdate()
+    {
+        net.FeedForward(inputs);
+
+        //find the last layer (x value, horizontalInput) and the first y value (node 0)
+        carController.horizontalInput = net.neurons[net.layers.Length-1][0];
+        //find the last layer (x value, verticalInput) and the second y value (node 1)
+        carController.verticalInput = net.neurons[net.layers.Length - 1][1];
+    }
+
+    void UpdateFitness()
+    {
+        SetFitness(
+            distanceTraveled + //total travelled distance
+            (nextCheckpointDist - //fixed value, calculated once when you hit a new checkpoint
+            (transform.position - nextCheckpoint.position).magnitude)); //incremental value, calculated as you move to next checkpoint
+    }
+
+    //saves the car's highest fitness score
+    void SetFitness(float _fitness)
+    {
+        //if car's (base) fitness is smaller than new fitness
+        if(fitness < _fitness)
+        {
+            //base fitness = new fitness... does not update if _fitness is smaller!! only save positive progress
+            fitness = _fitness;
+        }
+    }
+
+    //feeds the information into the inputs
+    float RaySensor(Vector3 pos, Vector3 direction, float length)
+    {
+        if (Physics.Raycast(pos, direction, out hit, length * rayRange, layerMask))
+        {
+            Debug.DrawRay(pos, direction * length * rayRange, Color.green);
+            return (rayRange * length - hit.distance) / (rayRange * length); //returns a value between 0 and one
+        }
+
+        else
+        {
+            Debug.DrawRay(pos, direction * length * rayRange, Color.red);
+            return 0;
+        }
     }
 }
---
+++ MANCINI_MachineLearning/Semaine Intensive Machine Learning/Assets/Scripts/Manager.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public int populationSize = 100;
+    public float trainingDuration = 10;
+    public float mutationRate = 1;
+
+    public Agent agentPrefab;
+    public Transform agentGroup;
+
+    Agent agent;
+
+    public List<Agent> agents = new List<Agent>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(InitCoroutine());
+    }
+
+    IEnumerator InitCoroutine()
+    {
+        NewGeneration();
+        //InitNeuralNetworkViewer();
+        Focus();
+        yield return new WaitForSeconds(trainingDuration);
+        StartCoroutine(Loop());
+    }
+    
+    IEnumerator Loop()
+    {
+        NewGeneration();
+        Focus();
+        yield return new WaitForSeconds(trainingDuration);
+        StartCoroutine(Loop());
+    }
+
+    void NewGeneration()
+    {
+        //are sorted thanks to the IComparable interface in the agents class
+        agents.Sort();
+        AddOrRemoveAgent();
+        Mutate();
+        ResetAgent();
+        SetColor();  
+    }
+
+    //look at agent list, and add or remove according to population size
+    private void AddOrRemoveAgent()
+    {
+        if (agents.Count != populationSize)
+        {
+            int diff = populationSize - agents.Count;
+
+            if (agents.Count <= populationSize) for (int i = 0; i < diff; i++) AddAgent();
+            if (agents.Count >= populationSize) for (int i = 0; i < diff; i++) RemoveAgent();
+        }
+    }
+
+    private void AddAgent()
+    {
+        agent = Instantiate(agentPrefab, Vector3.zero, Quaternion.identity, agentGroup);
+        agent.net = new NeuralNetwork(agent.net.layers);
+
+        agents.Add(agent);
+    }
+
+    private void RemoveAgent()
+    {
+        //Destroy the object at the end of the list
+        Destroy(agents[agents.Count - 1]);
+        agents.RemoveAt(agents.Count - 1);
+    }
+
+    //Mutate the bottom half of the list (50 "worst" performing agents)
+    private void Mutate()
+    {
+        //for each agent in the bottom half (agents.Count/2)...
+        for (int i = agents.Count/2; i < agents.Count; i++)
+        {
+            //...copy the axon/Neural Network of the corresponding agent in the first half (51st copies 1st, 52nd copies 2nd...)
+            agents[i].net.CopyNet(agents[i - agents.Count/2].net);
+            agents[i].net.Mutate(mutationRate);
+            agents[i].SetMutatedColor();
+        }
+    }
+
+    private void ResetAgent()
+    {
+        for (int i = 0; i < agents.Count; i++)
+        {
+            agents[i].ResetAgent();
+        }
+    }
+
+    private void SetColor()
+    {
+        //start the for loop at the second element (agents[1])
+        for (int i = 1; i < agents.Count/2; i++)
+            agents[i].SetDefaultColor();
+
+        //set the color of the agent in first place
+        agents[0].SetFirstColor();
+    }
+    private void Focus()
+    {
+        //NeuralNetworkViewer.instance.agent = agents[0];
+        //NeuralNetworkViewer.instance.RefreshAxon();
+
+        CameraController.instance.target = agents[0].transform;
+    }
+}
